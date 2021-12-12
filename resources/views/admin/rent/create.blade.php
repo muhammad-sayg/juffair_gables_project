1df192629abe492327a23f75ef9ee033ec8735a9
HP <HP@DESKTOP-LLA9PFJ>
2021-12-12 16:58:43 +0300

    rent changes

--- resources/views/admin/rent/create.blade.php
+++ resources/views/admin/rent/create.blade.php
@@ -3,8 +3,9 @@
 
 {{-- page level styles --}}
 @section('header_styles')
-<link rel="stylesheet" href="{{asset('public/admin/assets/bundles/bootstrap-daterangepicker/daterangepicker.css') }}">
+{{-- <link rel="stylesheet" href="{{asset('public/admin/assets/bundles/bootstrap-daterangepicker/daterangepicker.css') }}"> --}}
 <link rel="stylesheet" href="https://code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker.min.css" integrity="sha512-mSYUmp1HYZDFaVKK//63EcZq4iFWFjxSL+Z3T/aCt4IO9Cejm03q3NKKYN6pFQzY0SBOr8h+eCIAZHPXcpZaNw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 <style>
 </style>
 @stop
@@ -89,7 +90,12 @@
                       <div class="col-lg-4">
                           <div class="form-group">
                             <label for="">Rent Month</label>
-                            <input type="text" name="rent_month" id="datepick" class="form-control datepicker1">
+                            <div class="input-group date" id="datetimepicker1" data-target-input="nearest">
+                              <input type="text" class="form-control datetimepicker-input" data-target="#datetimepicker1"/>
+                              <div class="input-group-append" data-target="#datetimepicker1" data-toggle="datetimepicker">
+                                  <div class="input-group-text"><i class="fa fa-calendar"></i></div>
+                              </div>
+                            </div>
                           </div>
                       </div>
                     </div>
@@ -101,13 +107,23 @@
                           <div class="col-lg-4">
                             <div class="form-group">
                               <label for="">Rent From</label>
-                              <input type="text" name="rent_from" class="form-control datepicker">
+                              <div class="input-group date" id="datetimepicker2" data-target-input="nearest">
+                                <input type="text" class="form-control datetimepicker-input" data-target="#datetimepicker2"/>
+                                <div class="input-group-append" data-target="#datetimepicker2" data-toggle="datetimepicker">
+                                    <div class="input-group-text"><i class="fa fa-calendar"></i></div>
+                                </div>
+                              </div>
                             </div>
                           </div>
                           <div class="col-lg-4">
                             <div class="form-group">
                               <label for="">Rent To</label>
-                              <input type="month" name="rent_to" class="form-control">
+                              <div class="input-group date" id="datetimepicker3" data-target-input="nearest">
+                                <input type="text" class="form-control datetimepicker-input" data-target="#datetimepicker3"/>
+                                <div class="input-group-append" data-target="#datetimepicker3" data-toggle="datetimepicker">
+                                    <div class="input-group-text"><i class="fa fa-calendar"></i></div>
+                                </div>
+                              </div>
                             </div>
                           </div>
                         </div>
@@ -132,7 +148,10 @@
 
 @stop
 @section('footer_scripts')
-<script src="{{asset('public/admin/assets/bundles/bootstrap-daterangepicker/daterangepicker.js') }}"></script>
+{{-- <script src="{{asset('public/admin/assets/bundles/bootstrap-daterangepicker/daterangepicker.js') }}"></script> --}}
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js" integrity="sha512-qTXRIMyZIFb8iQcfjXWCO8+M5Tbc38Qi5WzdPOYZHIlZpzBHG3L3by84BBBOiRGiEb7KKtAOAs5qYdUiZiQNNQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+<script src="https://foodlabriffa.ebahrain.biz/assets/bower_components/datepicker/js/bootstrap-datepicker.min.js"></script>
 <script>
   function getRentDetails(id) {
     $.get({
@@ -173,9 +192,31 @@
   });
 </script>
 
-<script>
-  $(document).ready(function() {
-	$('#datepick').datepicker()
-});
+<script type="text/javascript">
+  $(function () {
+      $('#datetimepicker1').datepicker({
+        format:'mm-yyyy',
+        viewMode: "months", 
+        minViewMode: "months", 
+        autoclose:true
+
+      });
+      
+      $('#datetimepicker2').datepicker({
+        format:'mm-yyyy',
+        viewMode: "months", 
+        minViewMode: "months", 
+        autoclose:true
+
+      });
+
+      $('#datetimepicker3').datepicker({
+        format:'mm-yyyy',
+        viewMode: "months", 
+        minViewMode: "months", 
+        autoclose:true
+
+      });
+  });
 </script>
 @stop
